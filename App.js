073b72b9bddef64c2061ef732dc445d1f72a65de
MarShin <martinshin95@gmail.com>
2018-05-05 01:29:48 +0800

    fix icon not showing bug

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-import React from "react";
-import Root from "./app/index";
+import React from 'react';
+import Root from './app/index';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Root />
-    );
-  }
-}
+const App = () => <Root />;
+
+export default App;
--- app/components/Button/IconButton.js
+++ app/components/Button/IconButton.js
@@ -1,28 +1,64 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, TouchableOpacity, Image, View } from 'react-native';
-import { Icon } from 'react-native-elements';
+// import { Icon } from 'react-native-elements';
 
 import styles from './styles';
 
-const IconButton = ({ iconName, text, onPress }) => (
-  <TouchableOpacity style={styles.container} onPress={onPress}>
-    <View style={styles.wrapper}>
-      <Image
-        resizeMode="contain"
-        style={styles.icon}
-        source={require('./images/${iconName}.png')}
-      />
-      {/* <Icon name="cycle" size={35} /> */}
-      <Text style={styles.text}>{text}</Text>
-    </View>
-  </TouchableOpacity>
-);
+// const IconButton = ({ iconName, text, onPress }) => (
+
+class IconButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleButtonPress = () => {
+    console.log('press icon button~');
+  };
+
+  render() {
+    // const temp = this.props.iconName;
+    // const name = `./images/${{ temp }}.png`;
+
+    let filename = null;
+    switch (this.props.iconName) {
+      default:
+        filename = require('./images/icon.png');
+        break;
+
+      case 'convert':
+        filename = require('./images/convert.png');
+        break;
+
+      case 'price':
+        filename = require('./images/price.png');
+        break;
+
+      case 'receive_money':
+        filename = require('./images/receive_money.png');
+        break;
+
+      case 'send_money':
+        filename = require('./images/send_money.png');
+        break;
+    }
+
+    return (
+      <TouchableOpacity style={styles.container} onPress={this.handleButtonPress}>
+        <View style={styles.wrapper}>
+          <Image resizeMode="contain" style={styles.icon} source={filename} />
+          {/* <Icon name="cycle" size={35} /> */}
+          <Text style={styles.text}>{this.props.text}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 IconButton.propTypes = {
   iconName: PropTypes.string,
   text: PropTypes.string,
-  onPress: PropTypes.func,
+  // onPress: PropTypes.func,
 };
 
 export default IconButton;
